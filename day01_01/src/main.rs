02aebd0247a3f24c251ee02773626f051fbf2625
thomas-b <breleur.t@gmail.com>
2019-12-01 21:37:29 +0100

    parse the line into numbers then perform a map reduce on them to process the right answer: 3287899

--- day01_01/src/main.rs
+++ day01_01/src/main.rs
@@ -11,13 +11,29 @@ fn main() {
 		}
 	};
 	let reader = BufReader::new(file);
-	let module_weights = vec!<i32>();
+	let mut module_weights: Vec<i32> = Vec::new();
+
 	for line in reader.lines() {
 		let line = match line {
 			Ok(line) => line,
 			Err(error) => panic!("Couldn't read line got error: {:?}", error),
 		};
 
-		println!("{}", line)
+		let module_weight = line.parse::<i32>();
+		let module_weight = match module_weight {
+			Ok(m) => m,
+			Err(error) => panic!("Couldn't parse line into number got error: {:?}", error),
+		};
+
+		module_weights.push(module_weight);
 	}
+	let per_module_fuel_requirement: i32 = module_weights
+		.into_iter()
+		.map(calculate_fuel_requirement)
+		.fold(0, |acc, x| acc + x);
+	println!("{:?}", per_module_fuel_requirement);
+}
+
+fn calculate_fuel_requirement(module_weight: i32) -> i32 {
+	(module_weight / 3) - 2
 }
